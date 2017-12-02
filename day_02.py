abda18e952408c9cd767389f9fecd3253d354342
Johan Hägg <kaustic@gmx.co.uk>
2017-12-02 20:22:22 +0100

    Day 2, solution
    Behold the abomination!

--- day_02.py
+++ day_02.py
@@ -1,2 +1,14 @@
+import sys
+
+try:
+    from functools import reduce
+except ImportError:
+    pass
+
+
 def day_02(spreadsheet):
-    return -1
+    return sum(a - b for a, b in
+               (reduce(lambda (a, b), e: (max(e, a), min(e, b)),
+                       row,
+                       (-sys.maxint, sys.maxint))
+               for row in spreadsheet))
