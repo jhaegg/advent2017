a66c25ea4cda3e6cae21ef1973c0dbc3200d130c
Johan Hägg <kaustic@gmx.co.uk>
2017-12-03 15:57:10 +0100

    Day 1, did not notice there was a part 2

--- day_01.py
+++ day_01.py
@@ -1,7 +1,10 @@
 import sys
 
-def day_01(captcha):
-    return sum(int(c) for i, c in enumerate(captcha) if c == captcha[i-1])
+def day_01(captcha, offset):
+    return sum(int(c) for i, c in enumerate(captcha) if c == captcha[(i + offset) % len(captcha)])
 
 if __name__ == '__main__':
-    print(day_01(sys.argv[1]))
+    if len(sys.argv) == 2:
+        print(day_01(sys.argv[1], len(sys.argv[1]) // 2))
+    else:
+        print(day_01(sys.argv[1], int(sys.argv[2])))
--- test/test_day_01.py
+++ test/test_day_01.py
@@ -3,15 +3,22 @@ from day_01 import day_01
 
 class TestDay01(TestCase):
     def test_sum_consecutive(self):
-        self.assertEqual(3, day_01("1122"))
+        self.assertEqual(3, day_01("1122", -1))
 
     def test_sum_multiple(self):
         # Replacement for example 2, this one does not assume wraparound
-        self.assertEqual(2, day_01("1112"))
+        self.assertEqual(2, day_01("1112", -1))
 
     def test_none(self):
-        self.assertEqual(0, day_01("1234"))
+        self.assertEqual(0, day_01("1234", -1))
 
     def test_wraparound(self):
-        self.assertEqual(9, day_01("91212129"))
-        self.assertEqual(4, day_01("1111"))
+        self.assertEqual(9, day_01("91212129", -1))
+        self.assertEqual(4, day_01("1111", -1))
+
+    def test_part2(self):
+        self.assertEqual(6, day_01("1212", 2))
+        self.assertEqual(0, day_01("1221", 2))
+        self.assertEqual(4, day_01("123425", 3))
+        self.assertEqual(12, day_01("123123", 3))
+        self.assertEqual(4, day_01("12131415", 4))
